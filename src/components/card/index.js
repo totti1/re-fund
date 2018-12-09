c72148c5a0554555839272e003194a96153b79db
Katy Aristote <stotestorch@gmail.com>
2018-12-09 11:55:29 +0200

    last commit

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
 import "./style/cardstyle.css";
 // import styles from "./style/styles";
@@ -11,25 +11,28 @@ import "./style/cardstyle.css";
 // const selected = ()=> {}
 
 const Card = props => {
-  //   const { selected, selected2, selected3 } = props;
+  const { title, description, onClick } = props;
   return (
     <div
       class="card mycard"
       style={{ width: "60%", marginTop: 20, alignSelf: "center" }}
     >
       <div class="card-body">
-        <h5 class="card-title">Title</h5>
-        <p class="card-text">Small Description</p>
-        <Link to="/projet+realises">
-          <button type="button" class="btn btn-outline-dark">
-            See more
-          </button>
-        </Link>
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{description}</p>
+        {/* <Link to="/projet+realises"> */}
+        <button type="button" class="btn btn-outline-dark" onClick={onClick}>
+          See more
+        </button>
+        {/* </Link> */}
       </div>
     </div>
   );
 };
 Card.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  onClick: PropTypes.func
   //   selected: PropTypes.bool,
   //   selected2: PropTypes.bool,
   //   selected3: PropTypes.bool
--- src/screens/business_management/index.js
+++ src/screens/business_management/index.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
 import Menu from "../../components/menu";
 import { Link } from "react-router-dom";
+import * as firebase from "firebase";
+import _ from "lodash";
 // import "./styles/login.css";
 
 class Business_Management extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      business: []
+    };
+  }
+  async componentWillMount() {
+    const that = this;
+    await firebase
+      .database()
+      .ref(`/business`)
+      .on("value", snapshot => {
+        // console.log(snapshot)
+        const usersData = _.map(snapshot.val(), (val, uid) => {
+          return { ...val, uid };
+        });
+        // console.log(usersData);
+        that.setState({
+          business: usersData
+          // loading: false,
+        });
+      });
+  }
   render() {
     return (
       <div>
@@ -18,19 +43,33 @@ class Business_Management extends Component {
             }}
           >
             <div class="card-body">
-              <Link
-                to="/business+analysis"
-                style={{ textDecoration: "none", color: "black" }}
-              >
-                <h5 class="card-title">Business Name</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </Link>
+              {this.state.business.map((item, key) => {
+                // console.log(item);
+                return (
+                  <div
+                    class="card"
+                    style={{
+                      width: "60%",
+                      elevation: 5,
+                      margin: "0 auto",
+                      marginBottom: 20
+                    }}
+                  >
+                    <div class="card-body">
+                      <Link
+                        to="/business+analysis"
+                        style={{ textDecoration: "none", color: "black" }}
+                      >
+                        <h5 class="card-title">{item.business_title}</h5>
+                        <p class="card-text">{item.description}</p>
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
-          <div
+          {/* <div
             class="card"
             style={{
               width: "60%",
@@ -95,7 +134,7 @@ class Business_Management extends Component {
                 </p>
               </Link>
             </div>
-          </div>
+          </div> */}
         </Menu>
       </div>
     );
